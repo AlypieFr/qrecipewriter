d5e1d3c8574e9acf6b8ff396436e70b4370546b4
Floreal Cabanettes <floreal.cabanettes@lirmm.fr>
2016-02-21 22:43:22 +0100

    Add check no connexion internet [not only] for auto search updates

--- searchupdates.cpp
+++ searchupdates.cpp
@@ -33,46 +33,51 @@ void SearchUpdates::start() {
 void SearchUpdates::replyFinished(QNetworkReply *pReply)
 {
     QString version (pReply->readAll());
-    QStringList lines = version.split("\n");
-    version = lines[0].replace("\r", "");
-    QStringList lVerNb = version.split(".");
-    int i = 0;
-    bool isVersionCorrect = true;
-    while (i < lVerNb.length() && isVersionCorrect) {
-        bool ok;
-        lVerNb[i].toInt(&ok);
-        if (!ok) {
-            isVersionCorrect = false;
+    if (version != "") {
+        QStringList lines = version.split("\n");
+        version = lines[0].replace("\r", "");
+        QStringList lVerNb = version.split(".");
+        int i = 0;
+        bool isVersionCorrect = true;
+        while (i < lVerNb.length() && isVersionCorrect) {
+            bool ok;
+            lVerNb[i].toInt(&ok);
+            if (!ok) {
+                isVersionCorrect = false;
+            }
+            i++;
         }
-        i++;
-    }
-    if (isVersionCorrect) {
-        QStringList tVerNb = VERSION.split(".");
-        bool hasUpdate = false;
-        int niv = 0;
-        while (!hasUpdate && niv < qMin(lVerNb.size(), tVerNb.size()))
-        {
-            if (lVerNb[niv] > tVerNb[niv])
-                hasUpdate = true;
-            else if(lVerNb[niv] < tVerNb[niv])
-                niv = qMin(lVerNb.size(), tVerNb.size()); //We stop the loop
-            niv++;
-        }
-        if (hasUpdate) {
-            if (hasUpdate)
+        if (isVersionCorrect) {
+            QStringList tVerNb = VERSION.split(".");
+            bool hasUpdate = false;
+            int niv = 0;
+            while (!hasUpdate && niv < qMin(lVerNb.size(), tVerNb.size()))
             {
-                //Propose to user downloading the last version
-                int rep = QMessageBox::information(_parent, "Mise à jour disponible", QString("Une mise à jour du logicielle est disponible, voulez-vous ")
-                                                   + QString("mettre à jour votre version ") + VERSION + QString(" à la dernière version (" + version + ") ?"), QMessageBox::Yes, QMessageBox::No);
-                if (rep == QMessageBox::Yes)
+                if (lVerNb[niv] > tVerNb[niv])
+                    hasUpdate = true;
+                else if(lVerNb[niv] < tVerNb[niv])
+                    niv = qMin(lVerNb.size(), tVerNb.size()); //We stop the loop
+                niv++;
+            }
+            if (hasUpdate) {
+                if (hasUpdate)
                 {
-                    QString adresse = lines[1].left(lines[1].length() - 1);
-                    Functions::downloadUpdate(adresse, _parent);
+                    //Propose to user downloading the last version
+                    int rep = QMessageBox::information(_parent, "Mise à jour disponible", QString("Une mise à jour du logicielle est disponible, voulez-vous ")
+                                                       + QString("mettre à jour votre version ") + VERSION + QString(" à la dernière version (" + version + ") ?"), QMessageBox::Yes, QMessageBox::No);
+                    if (rep == QMessageBox::Yes)
+                    {
+                        QString adresse = lines[1].left(lines[1].length() - 1);
+                        Functions::downloadUpdate(adresse, _parent);
+                    }
                 }
             }
         }
+        else {
+            qWarning() << "Error: unable to check version, the newest version from the net was not correct";
+        }
     }
     else {
-        qWarning() << "Error: unable to check version, the newest version from the net was not correct";
+        qWarning() << "Warning: unable to check for updates. May be you are not connecter to the internet.";
     }
 }
