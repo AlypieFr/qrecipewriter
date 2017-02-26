9a782e1798ed161152ab3845bdc57b729f9ce9d4
Floréal Cabanettes <floreal.cab@gmail.com>
2017-02-26 20:08:33 +0100

    Update copyright headers

--- headers/spellcheckdialog.h
+++ headers/spellcheckdialog.h
@@ -1,7 +1,15 @@
 /*
- * © 2013-2016 Flo-Art.fr
+ * © Copyright 2013-2017 Flo-Art.fr
  *
- * QRecipeWriter et l'ensemble de ses putils est fournit sous la licence Creative Common BY-NC-SA.
+ * QRecipeWriter is available under the Creative Common BY-NC-SA licence.
+ * All modifications and redistributions on the code are allowed for a NON COMMERCIAL usage.
+ * Also, modifications and reproductions of the code must agree these conditions:
+ *    - This copyright header must NOT be deleted.
+ *    - You MUST redistribute the modified or not version under the Creative Common licence at least as restrictive as ours.
+ *    - Flo-Art.fr can't be responsible for any version modified or redistributed by third parties.
+ *    - Any commercial usage of this code, full or partial, is forbidden.
+ *
+ * QRecipeWriter est fournit sous la licence Creative Common BY-NC-SA.
  * Toutes les modifications et la redistribution sont autorisés pour une utilisation NON COMMERCIALE.
  * Par ailleurs, les modifications et la reproduction doivent respecter les règles ci-dessous :
  *    - Cette en-tête doit être maintenue.
@@ -10,7 +18,7 @@
  *    - Flo-Art.fr ne peut être tenu pour responsable des versions modifiées et/ou redistribuées.
  *    - Toute utilisation commerciale partielle ou complète est interdite.
  *
- * Initial code by Volker Götz, modified by Conseils En Cusine !
+ * Initial code by Volker Götz, modified by Flo-Art.fr
  */
 
 /*
--- src/sendpywebcooking.cpp
+++ src/sendpywebcooking.cpp
@@ -1,3 +1,24 @@
+/*
+ * © Copyright 2013-2017 Flo-Art.fr
+ *
+ * QRecipeWriter is available under the Creative Common BY-NC-SA licence.
+ * All modifications and redistributions on the code are allowed for a NON COMMERCIAL usage.
+ * Also, modifications and reproductions of the code must agree these conditions:
+ *    - This copyright header must NOT be deleted.
+ *    - You MUST redistribute the modified or not version under the Creative Common licence at least as restrictive as ours.
+ *    - Flo-Art.fr can't be responsible for any version modified or redistributed by third parties.
+ *    - Any commercial usage of this code, full or partial, is forbidden.
+ *
+ * QRecipeWriter est fournit sous la licence Creative Common BY-NC-SA.
+ * Toutes les modifications et la redistribution sont autorisés pour une utilisation NON COMMERCIALE.
+ * Par ailleurs, les modifications et la reproduction doivent respecter les règles ci-dessous :
+ *    - Cette en-tête doit être maintenue.
+ *    - Vous devez redistribuer la version modifiée ou non sous licence Creative Common au moins autant
+ *      restrictive.
+ *    - Flo-Art.fr ne peut être tenu pour responsable des versions modifiées et/ou redistribuées.
+ *    - Toute utilisation commerciale partielle ou complète est interdite.
+ */
+
 #include "sendpywebcooking.h"
 
 extern QHash<int,QHash<QString, QString>> serverConfs;
